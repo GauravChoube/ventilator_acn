226368c86db93c872f02b6c1957a7ede116fa7ba
UmaNeeraj <dxcumaneeraj1@gmail.com>
2022-12-01 16:51:47 +0530

    Driven logic in Getstep

--- stepperMotor.cpp
+++ stepperMotor.cpp
@@ -27,6 +27,27 @@ int stepperMotorSetStep(unsigned int stepValue)
 }
 unsigned int stepperMotorGetStep()
 {
+    digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  
+    for(int x = 0; x < t; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(1000); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(1000); 
+    }
+    delay(1000);// One second delay
+  
+    digitalWrite(dirPin,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+  
+    for(int x = 0; x < t; x++) {
+        digitalWrite(stepPin,HIGH);
+        delayMicroseconds(u);
+        digitalWrite(stepPin,LOW);
+        delayMicroseconds(u);
+    }
+    delay(1000);
 
 }
 
